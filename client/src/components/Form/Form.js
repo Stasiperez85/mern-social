95b92044456161adfa3f19e5c1b4f7be50e1b495
Stasiperez85 <Stasiperez85@gmail.com>
2022-11-17 22:00:10 -0700

    make corrections

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -6,11 +6,11 @@ import { useDispatch } from 'react-redux';
 // GET CURRENT ID OF POST
 
 import useStyles from './styles';
-import { createPost, updatePost} from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
 const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
-    
+
     const classes = useStyles();
     const dispatch = useDispatch();
 
@@ -31,7 +31,7 @@ const Form = ({ currentId, setCurrentId }) => {
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variate="h6">Creating a Memory</Typography>
+                <Typography variant="h6">Creating a Memory</Typography>
                 <TextField name='creator' variant='outlined' label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name='title' variant='outlined' label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name='message' variant='outlined' label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
